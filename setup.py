418fb1a4d1f1d19108f8ad3265cb26500adfc6e8
huangning <huangning.honey@bytedance.com>
2020-09-13 16:06:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except IOError:
 
 setup(
       name=''+NAME,
-      version='1.0.0',
+      version='1.0.1',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type="text/markdown",
